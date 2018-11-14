ce0ab507ba8768ded1c45d469b1e407560eb6f40
Ben Drucker <bvdrucker@gmail.com>
2018-11-13 21:28:53 -0800

    standard

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-'use strict';
+'use strict'
 
 exports.epley = function epley (weight, reps) {
-  return weight * (1 + reps / 30);
-};
+  return weight * (1 + reps / 30)
+}
 
 exports.brzycki = function brzycki (weight, reps) {
-  return weight * 36 / (37 - reps);
-};
+  return weight * 36 / (37 - reps)
+}
 
 exports.lander = function lander (weight, reps) {
-  return (100 * weight) / (101.3 - 2.67123 * reps);
-};
+  return (100 * weight) / (101.3 - 2.67123 * reps)
+}
 
 exports.lombardi = function lombardi (weight, reps) {
-  return weight * Math.pow(reps, 0.10);
-};
+  return weight * Math.pow(reps, 0.10)
+}
 
 exports.mayhew = function mayhew (weight, reps) {
-  return (100 * weight) / (52.2 + 41.9 * Math.exp(-0.055 * reps));
-};
+  return (100 * weight) / (52.2 + 41.9 * Math.exp(-0.055 * reps))
+}
 
 exports.oconner = function oconner (weight, reps) {
-  return weight * (1 + 0.025 * reps);
-};
+  return weight * (1 + 0.025 * reps)
+}
 
 exports.wathan = function wathan (weight, reps) {
-  return (100 * weight) / (48.8 + 53.8 * Math.exp(-0.075 * reps));
-};
+  return (100 * weight) / (48.8 + 53.8 * Math.exp(-0.075 * reps))
+}
--- test/index.js
+++ test/index.js
@@ -1,10 +1,11 @@
-'use strict';
+'use strict'
 
-var expect = require('chai').expect;
-var rm     = require('../');
+var expect = require('chai').expect
+var rm = require('../')
+
+/* global describe, it */
 
 describe('1rm', function () {
-
   // 400# x 4
   var expectations = {
     epley: 453,
@@ -13,13 +14,12 @@ describe('1rm', function () {
     lombardi: 459,
     mayhew: 466,
     oconner: 440,
-    wathan: 451 
-  };
+    wathan: 451
+  }
 
   Object.keys(expectations).forEach(function (method) {
     it('can estimate with the #' + method + ' method', function () {
-      expect(rm[method](400, 4)).to.be.closeTo(expectations[method], 1);
-    });
-  });
-
-});
+      expect(rm[method](400, 4)).to.be.closeTo(expectations[method], 1)
+    })
+  })
+})
